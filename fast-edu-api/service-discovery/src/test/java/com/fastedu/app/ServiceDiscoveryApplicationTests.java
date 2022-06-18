f9333e0d3f3a593e0d6dd7a96640a82ea39c1606
vuongtranminh <vuongtranminh.hn@gmail.com>
2022-06-18 17:01:03 +0700

    init fast-edu-api

---
+++ fast-edu-api/service-discovery/src/test/java/com/fastedu/app/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastedu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
